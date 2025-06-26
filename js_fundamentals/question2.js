7e1fa844d695566115a1e2b2c70163070bdf1c10
Rohan Dangal <johndangal2061@gmail.com>
2025-06-26 21:36:05 +0545

    Added question.js

---
+++ js_fundamentals/question2.js
@@ -0,0 +1,15 @@
+// Given a number, you have to use the if statement to print "Big" (without quotes) if the given number is greater than 100, and use the else statement to print "Number" (without quotes) when the number is smaller than or equal to 100.
+// Note: You should print the newline after printing"Number" or"Big".
+
+let number = parseInt(prompt("Enter a number:"));
+
+if (number > 100) {
+    console.log("Big");
+} else {
+    console.log("Number");
+}
+
+
+
+
+
