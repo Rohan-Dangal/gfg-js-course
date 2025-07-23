594cf0817a197b72c8d6642991c0df2c4b81e228
Rohan Dangal <johndangal2061@gmail.com>
2025-07-23 13:33:52 +0545

    program to check the length of the string

---
+++ string methods/length_of_str.js
@@ -0,0 +1,13 @@
+// You are given a string s. You need to find the length of the string and return it.
+
+// Examples:
+
+// Input: s = Geeks
+// Output: 5
+
+function lengthof(s)
+{
+    return s.length;
+}
+const message = prompt("enter the string you want to check:");
+console.log("length of entered string is:",lengthof(message));
