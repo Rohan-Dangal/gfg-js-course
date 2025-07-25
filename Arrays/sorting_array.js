7587a4dc205dee7ad6f4b9fe9af66efc901e9892
Rohan Dangal <johndangal2061@gmail.com>
2025-07-25 11:15:36 +0545

    checking if the sorted array is in non-decreasing order

--- Arrays/sorting_array.js
+++ Arrays/sorting_array.js
@@ -1,1 +1,19 @@
 // given an array arr[]. check whether it is sorted in non-decreasing order. return true if its sorted otherwise false
+function checksortedarray(arr)
+{
+    for(let i=0;i<arr.length-1;i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            return false;
+        }
+        
+    }
+    return true;
+    
+}
+const arr1 = [5,10,15,20,25];
+const arr2 = [12,23,32,21,45];
+
+console.log(checksortedarray(arr1));
+console.log(checksortedarray(arr2));
